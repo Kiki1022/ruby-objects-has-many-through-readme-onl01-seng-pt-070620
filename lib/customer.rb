f7ba1b3bc49b391ddeb1cfbfd5c8d37b990db3b9
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-08 03:00:54 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,7 +14,7 @@ class Customer
     @@all
   end
 
-  def new_meal(waiter, total, tip=0) #
+  def new_meal(waiter, total, tip=0) # initializes a meal using the current Customer instance, a provided                                        Waiterinstance and a total and tip
     Meal.new(waiter, self, total, tip)
     waiter
   end
