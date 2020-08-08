e31207a062f89ec756135f518f902c2c30f0390f
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-08 01:30:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,7 +19,7 @@ class Waiter
     Meal.new(self, customer, total, tip)
   end
   
-  # def new_meal(customer, total, tip=0)
-  #   Meal.new(self, customer, total, tip)
-  # end
+  def meals
+    Meal.all.select {|m| m.waiter == self}
+  end  
 end
