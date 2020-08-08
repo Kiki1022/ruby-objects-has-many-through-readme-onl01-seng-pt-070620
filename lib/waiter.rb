5657d65d10de155cc90aa99378ee0fa508b2a9bf
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-08 00:31:05 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,7 +16,10 @@ class Waiter
   end
     
   def new_meal(customer, total, tip = 0) 
-    Meal.new()
+    Meal.new(self, customer, total, tip)
   end
   
+  # def new_meal(customer, total, tip=0)
+  #   Meal.new(self, customer, total, tip)
+  # end
 end
